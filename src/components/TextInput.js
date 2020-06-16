77c3e5687d239a28f89f9e9eeb4987694f8d0e4a
Sarah <40401149+arghmatey@users.noreply.github.com>
2020-06-16 16:37:54 -0700

    Create TextInput component

---
+++ src/components/TextInput.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TextInput extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <input placeholder='Neon Glow'></input>
+            </div>
+        )
+    }
+}
+
+export default TextInput;
