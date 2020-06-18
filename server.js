c3265ce67cda3bc534db366cfb7c0b2b921c3c13
Sarah <40401149+arghmatey@users.noreply.github.com>
2020-06-18 13:07:23 -0700

    deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const logger = require('morgan');
 
 const app = express();
@@ -6,8 +7,8 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
-const port = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(port, function () {
-    console.log(`Express app running on port ${port}`)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
