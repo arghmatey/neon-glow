56be99c6a8c893e51925fe43b062d942a3a14abf
Sarah <40401149+arghmatey@users.noreply.github.com>
2023-08-09 23:56:03 -0700

    Updates

---
+++ precache-manifest.69f0500b2e59a6cdad33dd22d5819ff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56d6695a7061fc096886e60dee90008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61ff00c2d788cdebc097",
+    "url": "/static/css/main.b3f79975.chunk.css"
+  },
+  {
+    "revision": "140ae1134e893d510ae5",
+    "url": "/static/js/2.d5fdf457.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d5fdf457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ff00c2d788cdebc097",
+    "url": "/static/js/main.bee69339.chunk.js"
+  },
+  {
+    "revision": "88bdde5dfdd3367c215c",
+    "url": "/static/js/runtime-main.4c8a83f9.js"
+  },
+  {
+    "revision": "f9a74cff8d7bafb69040bb80d9355472",
+    "url": "/static/media/Clip.f9a74cff.ttf"
+  }
+]);
